302d35325b7a5ac847d145d1b33dce203ea42e46
zienaz <zie.naz1606@gmail.com>
2024-06-08 07:33:13 +0000

    add all dotfiles

---
+++ .config/nvim/lua/chadrc.lua
@@ -0,0 +1,13 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "tokyodark",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
